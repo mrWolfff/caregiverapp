78b888d869d02c3fff1d9064794809280701c1d7
lucas.wolf <lucasewolflew@gmail.com>
2026-02-22 11:07:56 -0300

    trying to connect frontend on railway

--- src/main/java/br/com/caregiverapp/config/CorsConfig.java
+++ src/main/java/br/com/caregiverapp/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:8081"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
